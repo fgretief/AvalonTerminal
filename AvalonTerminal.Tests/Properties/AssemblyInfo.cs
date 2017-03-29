381ce818856b484ba34da9062ea360975be33686
Francois Retief <fgretief@gmail.com>
2017-03-29 08:53:26 +0200

    Create a sample project.

--- AvalonTerminal.Tests/Properties/AssemblyInfo.cs
+++ AvalonTerminal.Tests/Properties/AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyProduct("AvalonTerminal")]
 [assembly: AssemblyCopyright("2017 Francois Retief")]
 [assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("en-US")]
+[assembly: AssemblyCulture("")]
 
 // Setting ComVisible to false makes the types in this assembly not visible 
 // to COM components.  If you need to access a type in this assembly from 
